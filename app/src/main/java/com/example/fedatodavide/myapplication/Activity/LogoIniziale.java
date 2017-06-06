dd94a0540944a32105bd04dc00ce2581a2d2e54f
fedatodavide <AAA>
2017-06-06 11:36:52 +0200

    AAA

--- app/src/main/java/com/example/fedatodavide/myapplication/Activity/LogoIniziale.java
+++ app/src/main/java/com/example/fedatodavide/myapplication/Activity/LogoIniziale.java
@@ -1,31 +1,27 @@
 package com.example.fedatodavide.myapplication.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.example.fedatodavide.myapplication.R;
 
-public class LogoIniziale extends AppCompatActivity {
-
+public class LogoIniziale extends Activity {
+    //METODO CHE MOSTRA PER TRE SECONDI IL LOGO DELL'APPLICAZIONE E POI LANCIA L'ACTIVITY DI LOGIN
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_iniziale);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        try {
+       /* try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
 
         Intent i = new Intent(getApplicationContext(), Login.class);
-        startActivity(i);
-        finish();
+        //startActivity(i);
+        //finish();
     }
 }
